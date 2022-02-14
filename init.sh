db4dc8a5f0e0e57e3a9ee415d2d884cc766819f3
Tobias Effner <tobias.effner@valtech-mobility.com>
2022-02-14 15:04:59 +0100

    Initial Commit

---
+++ init.sh
@@ -0,0 +1,17 @@
+
+read -p "Enter the azure storage account key: " storage_key
+export AZURE_STORAGE_KEY=$storage_key
+
+read -p "Enter the azure storage account name: " storage_name
+export AZURE_STORAGE_ACCOUNT=$storage_name
+
+read -p "Enter the azure container name: " container_name
+pulumi login azblob://$container_name
+
+if [ -d "pulumidemo" ]; then
+  rm -rf pulumidemo
+fi
+
+mkdir pulumidemo
+
+echo "Done! Now go to pulumidemo and continue."
